c9053999515f8193e6c29ee896fd983e10330d58
Alex Merced <alexmerced@alexmerced.com>
2020-04-10 09:35:16 -0400

    added npm and module version

---
+++ sfuncmodule.js
@@ -0,0 +1,48 @@
+///////m-Blocks by Alex Merced of AlexMercedCoder.com
+
+//////////////
+//CaptureProps
+/////////////
+
+const captureProps = (element) => {
+    const att = [...element.attributes];
+    const entries = att.map((value) => {
+        return [value.name, value.value];
+    });
+
+    return Object.fromEntries(entries);
+};
+
+/////////////////////////
+//SuperFunc
+////////////////////////
+
+export const superFunc = (config) => {
+    const globals = {};
+    const creator = (id, initialState) => {
+        let state = initialState;
+        const target = document.querySelector(`[sfunc=${id}]`);
+        let props = captureProps(target);
+        target.innerHTML = config.builder(state, props, globals);
+        config.mount ? config.mount(state, props, target, globals) : null;
+
+        return [
+            (newState) => {
+                state = newState;
+                props = captureProps(target);
+                target.innerHTML = config.builder(state, props, globals);
+                config.update
+                    ? config.update(state, props, target, globals)
+                    : null;
+                return config.hookGen
+                    ? config.hookGen(state, props, target, globals)
+                    : null;
+            },
+            config.hookGen
+                ? config.hookGen(state, props, target, globals)
+                : null
+        ];
+    };
+
+    return creator;
+};
