fa885606af5fc56ee52163ae6c7c45b7533c0ce8
Alex Merced <alexmerced@alexmerced.com>
2020-04-10 20:05:48 -0400

    added assemble function

--- sfunc.js
+++ sfunc.js
@@ -18,18 +18,21 @@ const captureProps = (element) => {
 ////////////////////////
 
 const superFunc = (config) => {
-    const globals = {};
-    let state = config.state;
-
     const creator = (id, initialState) => {
+        const globals = {};
+        let state = config.state ? config.state : {};
         const target = document.querySelector(`[sfunc=${id}]`);
         let props = captureProps(target);
         target.innerHTML = config.builder(state, props, globals);
+        config.assemble ? config.assemble(state, props, target, globals) : null;
         config.mount ? config.mount(state, props, target, globals) : null;
         globals.setState = (newState) => {
             state = newState;
             props = captureProps(target);
             target.innerHTML = config.builder(state, props, target, globals);
+            config.assemble
+                ? config.assemble(state, props, target, globals)
+                : null;
             config.update ? config.update(state, props, target, globals) : null;
             return config.hookGen
                 ? config.hookGen(state, props, target, globals)
